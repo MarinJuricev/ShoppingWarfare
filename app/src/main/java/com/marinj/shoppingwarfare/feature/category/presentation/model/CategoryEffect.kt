b78fd07187ecde129a6ea35a430cdcd39bd06ae9
mjuric00@fesb.hr <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>
2021-08-30 21:32:43 +0200

    Run ktlint

--- app/src/main/java/com/marinj/shoppingwarfare/feature/category/presentation/model/CategoryEffect.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/category/presentation/model/CategoryEffect.kt
@@ -1,7 +1,7 @@
 package com.marinj.shoppingwarfare.feature.category.presentation.model
 
 sealed class CategoryEffect {
-    //TODO instead of categoryTitle introduce an ID
+    // TODO instead of categoryTitle introduce an ID
     data class DeleteCategory(val categoryTitle: String) : CategoryEffect()
     data class Error(val errorMessage: String) : CategoryEffect()
     data class NavigateToCategoryDetail(val categoryTitle: String) : CategoryEffect()
