983c9a0a588da660feb4b5754e53761f7d7ab7e8
marinjuricev <mjuric00@fesb.hr>
2021-09-04 18:24:32 +0200

    Create test coverage for CreateCategoryItem

--- app/src/main/java/com/marinj/shoppingwarfare/feature/categorydetail/domain/usecase/CreateCategoryItem.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/categorydetail/domain/usecase/CreateCategoryItem.kt
@@ -1,6 +1,8 @@
 package com.marinj.shoppingwarfare.feature.categorydetail.domain.usecase
 
 import com.marinj.shoppingwarfare.core.result.Either
+import com.marinj.shoppingwarfare.core.result.Either.Left
+import com.marinj.shoppingwarfare.core.result.Either.Right
 import com.marinj.shoppingwarfare.core.result.Failure
 import com.marinj.shoppingwarfare.feature.categorydetail.domain.model.CategoryItem
 import com.marinj.shoppingwarfare.feature.categorydetail.domain.repository.CategoryDetailRepository
@@ -8,13 +10,21 @@ import javax.inject.Inject
 
 class CreateCategoryItem @Inject constructor(
     private val validateCategoryItem: ValidateCategoryItem,
-    private val categoryDetailRepository: CategoryDetailRepository
+    private val uuidGenerator: () -> String,
+    private val categoryDetailRepository: CategoryDetailRepository,
 ) {
 
     suspend operator fun invoke(
-        categoryItem: CategoryItem,
+        categoryItemTitle: String?,
     ): Either<Failure, Unit> {
-
-        return categoryDetailRepository.upsertCategoryItem(categoryItem)
+        return when (val result = validateCategoryItem(categoryItemTitle)) {
+            is Left -> result
+            is Right -> categoryDetailRepository.upsertCategoryItem(
+                CategoryItem(
+                    id = uuidGenerator(),
+                    name = categoryItemTitle!!
+                )
+            )
+        }
     }
 }
--- app/src/test/java/com/marinj/shoppingwarfare/feature/categorydetail/domain/usecase/CreateCategoryItemTest.kt
+++ app/src/test/java/com/marinj/shoppingwarfare/feature/categorydetail/domain/usecase/CreateCategoryItemTest.kt
@@ -1,9 +1,87 @@
-package com.marinj.shoppingwarfare.feature.categorydetail.domain.usecase;
+package com.marinj.shoppingwarfare.feature.categorydetail.domain.usecase
 
-import static org.junit.Assert.*;
+import com.google.common.truth.Truth.assertThat
+import com.marinj.shoppingwarfare.core.result.Failure
+import com.marinj.shoppingwarfare.core.result.buildLeft
+import com.marinj.shoppingwarfare.core.result.buildRight
+import com.marinj.shoppingwarfare.feature.categorydetail.domain.model.CategoryItem
+import com.marinj.shoppingwarfare.feature.categorydetail.domain.repository.CategoryDetailRepository
+import io.mockk.coEvery
+import io.mockk.every
+import io.mockk.mockk
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
+import org.junit.Test
 
-public class CreateCategoryItemTest {
+private const val UUID = "id"
+private const val CATEGORY_ITEM_TITLE = "title"
 
-    public void setUp() {
+@ExperimentalCoroutinesApi
+class CreateCategoryItemTest {
+
+    private val validateCategoryItem: ValidateCategoryItem = mockk()
+    private val uuidGenerator: () -> String = mockk()
+    private val categoryDetailRepository: CategoryDetailRepository = mockk()
+
+    private lateinit var sut: CreateCategoryItem
+
+    @Before
+    fun setUp() {
+        every { uuidGenerator() } answers { UUID }
+
+        sut = CreateCategoryItem(
+            validateCategoryItem,
+            uuidGenerator,
+            categoryDetailRepository,
+        )
     }
+
+    @Test
+    fun `invoke should return Left when validateCategoryItem returns Left`() = runBlockingTest {
+        val left = Failure.Unknown.buildLeft()
+        coEvery {
+            validateCategoryItem(CATEGORY_ITEM_TITLE)
+        } coAnswers { left }
+
+        val actualResult = sut(CATEGORY_ITEM_TITLE)
+
+        assertThat(actualResult).isEqualTo(left)
+    }
+
+    @Test
+    fun `invoke should return Left when validateCategoryItem returns Right and CategoryDetailRepository returns Left`() =
+        runBlockingTest {
+            val repositoryLeft = Failure.Unknown.buildLeft()
+            val validatorRight = Unit.buildRight()
+            val categoryItem = CategoryItem(UUID, CATEGORY_ITEM_TITLE)
+            coEvery {
+                validateCategoryItem(CATEGORY_ITEM_TITLE)
+            } coAnswers { validatorRight }
+            coEvery {
+                categoryDetailRepository.upsertCategoryItem(categoryItem)
+            } coAnswers { repositoryLeft }
+
+            val actualResult = sut(CATEGORY_ITEM_TITLE)
+
+            assertThat(actualResult).isEqualTo(repositoryLeft)
+        }
+
+    @Test
+    fun `invoke should return Right when validateCategoryItem returns Right and CategoryDetailRepository returns Right`() =
+        runBlockingTest {
+            val repositoryRight = Unit.buildRight()
+            val validatorRight = Unit.buildRight()
+            val categoryItem = CategoryItem(UUID, CATEGORY_ITEM_TITLE)
+            coEvery {
+                validateCategoryItem(CATEGORY_ITEM_TITLE)
+            } coAnswers { validatorRight }
+            coEvery {
+                categoryDetailRepository.upsertCategoryItem(categoryItem)
+            } coAnswers { repositoryRight }
+
+            val actualResult = sut(CATEGORY_ITEM_TITLE)
+
+            assertThat(actualResult).isEqualTo(repositoryRight)
+        }
 }
