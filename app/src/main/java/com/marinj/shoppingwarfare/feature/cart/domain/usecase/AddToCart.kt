8c7131e39f697085401ea1432c822d4b509bac29
mjuric00@fesb.hr <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>
2021-09-11 23:58:22 +0200

    Start working on the AddToCart flow

---
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/cart/domain/usecase/AddToCart.kt
@@ -0,0 +1,10 @@
+package com.marinj.shoppingwarfare.feature.cart.domain.usecase
+
+import javax.inject.Inject
+
+class AddToCart @Inject constructor() {
+
+    suspend operator fun invoke() {
+
+    }
+}
