a085f6e4c246bff3cb1f70dd8faabaa0a250bd79
marinjuricev <mjuric00@fesb.hr>
2021-09-24 11:49:12 +0200

    Make HistoryPage and  UserPage take the full screensize

--- app/src/main/java/com/marinj/shoppingwarfare/feature/history/HistoryPage.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/history/HistoryPage.kt
@@ -1,8 +1,12 @@
 package com.marinj.shoppingwarfare.feature.history
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import com.marinj.shoppingwarfare.core.viewmodel.TopBarEvent
 
 @Composable
@@ -13,5 +17,10 @@ fun HistoryPage(
         setupTopBar(TopBarEvent.HistoryTopBar())
     }
 
-    Text("History")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("History")
+    }
 }
--- app/src/main/java/com/marinj/shoppingwarfare/feature/user/UserPage.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/user/UserPage.kt
@@ -1,9 +1,18 @@
 package com.marinj.shoppingwarfare.feature.user
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 
 @Composable
 fun UserPage() {
-    Text("User")
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Text("User")
+    }
 }
