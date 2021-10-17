71fa5d4a50b2d9ba07553b104ebbcdfae9a2609f
MarinJ <mjuric00@fesb.hr>
2021-10-18 00:46:21 +0200

    Add a hacky camera flash when the picture is taken

--- app/src/main/java/com/marinj/shoppingwarfare/feature/cart/presentation/components/CameraPreview.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/cart/presentation/components/CameraPreview.kt
@@ -9,13 +9,23 @@ import androidx.camera.core.ImageCaptureException
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.res.painterResource
@@ -26,6 +36,7 @@ import androidx.lifecycle.LifecycleOwner
 import com.google.common.util.concurrent.ListenableFuture
 import com.marinj.shoppingwarfare.R
 import com.marinj.shoppingwarfare.core.components.ShoppingWarfareIconButton
+import kotlinx.coroutines.delay
 import timber.log.Timber
 import java.io.File
 import java.text.SimpleDateFormat
@@ -34,6 +45,7 @@ import java.util.Locale
 private const val DATE_FORMAT = "yyyy:MM:dd HH:mm:ss"
 private const val JPG_EXTENSION = ".jpg"
 private const val ICON_ALPHA = 0.5f
+private const val CAMERA_FLASH_DURATION = 100L
 
 // https://stackoverflow.com/questions/61795508/how-can-i-use-a-cameraview-with-jetpack-compose
 @Composable
@@ -46,6 +58,22 @@ fun CameraPreview(
 ) {
     val lifecycleOwner = LocalLifecycleOwner.current
     val context = LocalContext.current
+    var triggerFlash by remember { mutableStateOf(false) }
+    val transition = updateTransition(
+        targetState = triggerFlash,
+        label = "flashAnimation"
+    )
+    val flashBackground by transition.animateColor(
+        label = "flashBackground"
+    ) { shouldFlash ->
+        if (shouldFlash) {
+            LaunchedEffect(key1 = shouldFlash) {
+                delay(CAMERA_FLASH_DURATION)
+                triggerFlash = false
+            }
+            Color.White
+        } else Color.Transparent
+    }
     var imageCapture: ImageCapture? = null
 
     Box {
@@ -86,6 +114,7 @@ fun CameraPreview(
                 setupImageCaptureListener(
                     imageCapture,
                     context,
+                    { triggerFlash = true },
                     onImageSaved,
                     onImageError,
                 )
@@ -97,6 +126,11 @@ fun CameraPreview(
                 contentDescription = null
             )
         }
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(flashBackground)
+        )
     }
 }
 
@@ -135,6 +169,7 @@ private fun setupCameraView(
     private fun setupImageCaptureListener(
         imageCapture: ImageCapture?,
         context: Context,
+        cameraTriggered: () -> Unit,
         onImageSaved: (String) -> Unit,
         onImageError: () -> Unit
     ) {
@@ -155,11 +190,13 @@ private fun setupCameraView(
             object : ImageCapture.OnImageSavedCallback {
                 override fun onImageSaved(output: ImageCapture.OutputFileResults) {
                     val savedUri = Uri.fromFile(photoFile)
+                    cameraTriggered()
                     onImageSaved(savedUri.toString())
                 }
 
                 override fun onError(exc: ImageCaptureException) {
                     Timber.d("Photo capture failed: ${exc.message}", exc)
+                    cameraTriggered()
                     onImageError()
                 }
             }
