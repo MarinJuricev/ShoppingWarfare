206f865b7e07d7a44f03f0d38d375ea99d6f9473
MarinJ <mjuric00@fesb.hr>
2021-11-10 00:43:09 +0100

    Change from consumeAsFlow to receiveAsFlow to not crash in orientationChanges

--- app/src/main/java/com/marinj/shoppingwarfare/core/navigation/Navigator.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/core/navigation/Navigator.kt
@@ -1,13 +1,13 @@
 package com.marinj.shoppingwarfare.core.navigation
 
 import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.flow.consumeAsFlow
+import kotlinx.coroutines.flow.receiveAsFlow
 
 // Heavily inspired from https://funkymuse.dev/posts/compose_hilt_mm/
 class Navigator {
 
     private val _navigationEvent = Channel<NavigationEvent>()
-    val navigationEvent = _navigationEvent.consumeAsFlow()
+    val navigationEvent = _navigationEvent.receiveAsFlow()
 
     suspend fun emitDestination(navigationEvent: NavigationEvent) {
         _navigationEvent.send(navigationEvent)
