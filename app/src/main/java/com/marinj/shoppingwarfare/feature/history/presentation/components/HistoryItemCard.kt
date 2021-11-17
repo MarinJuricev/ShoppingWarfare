8e9c901f79e2a595972ccb086ff15d79dd467236
MarinJ <mjuric00@fesb.hr>
2021-11-17 01:02:00 +0100

    Minor styling to History feature

--- app/src/main/java/com/marinj/shoppingwarfare/feature/history/presentation/components/HistoryItemCard.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/history/presentation/components/HistoryItemCard.kt
@@ -1,19 +1,58 @@
 package com.marinj.shoppingwarfare.feature.history.presentation.components
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import com.marinj.shoppingwarfare.R
+import com.marinj.shoppingwarfare.core.components.ShoppingWarfareSwipeToDismiss
 import com.marinj.shoppingwarfare.feature.history.presentation.model.UiHistoryItem
 
 @Composable
 fun HistoryItemCard(historyItem: UiHistoryItem) {
-    Column {
-        Text(text = historyItem.cartName)
-        Spacer(modifier = Modifier.height(16.dp))
-        Text(text = historyItem.date)
+    ShoppingWarfareSwipeToDismiss(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(horizontal = 8.dp),
+        onDismiss = { },
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Column {
+                HistoryItemDescriptionText(description = stringResource(id = R.string.cart_name))
+                HistoryItemBodyText(bodyText = historyItem.cartName)
+            }
+            Column {
+                HistoryItemDescriptionText(description = stringResource(id = R.string.history_item_date))
+                HistoryItemBodyText(bodyText = historyItem.date)
+            }
+        }
     }
+}
+
+@Composable
+fun HistoryItemDescriptionText(description: String) {
+    Text(
+        text = description,
+        style = MaterialTheme.typography.body2,
+        color = if (MaterialTheme.colors.isLight) Color.LightGray else Color.White
+    )
+}
+
+@Composable
+fun HistoryItemBodyText(bodyText: String) {
+    Text(
+        text = bodyText,
+        style = MaterialTheme.typography.body1,
+    )
 }
