d793d69e78ebb33115d22415747279f66f782b69
MarinJ <mjuric00@fesb.hr>
2022-01-16 23:50:01 +0100

    Use a Buffered channel for events

--- app/src/main/java/com/marinj/shoppingwarfare/core/theme/Theme.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/core/theme/Theme.kt
@@ -1,12 +1,10 @@
 package com.marinj.shoppingwarfare.core.theme
 
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material.Colors
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.darkColors
 import androidx.compose.material.lightColors
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.graphics.Color
 
 private val DarkColorPalette = darkColors(
     primary = purpleDark,
