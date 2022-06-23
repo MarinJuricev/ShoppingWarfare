9eb4fccd764a0bd6a8209915af39750631fc0433
marinjuricev <mjuric00@fesb.hr>
2022-06-23 18:41:20 +0200

    Remove the optional parameters in MainCorutineTestRule

--- app/src/test/java/com/marinj/shoppingwarfare/MainCoroutineRule.kt
+++ app/src/test/java/com/marinj/shoppingwarfare/MainCoroutineRule.kt
@@ -11,12 +11,12 @@ import org.junit.runner.Description
 class MainCoroutineRule(
     private val dispatcher: TestDispatcher = UnconfinedTestDispatcher()
 ) : TestWatcher() {
-    override fun starting(description: Description?) {
+    override fun starting(description: Description) {
         super.starting(description)
         Dispatchers.setMain(dispatcher)
     }
 
-    override fun finished(description: Description?) {
+    override fun finished(description: Description) {
         super.finished(description)
         Dispatchers.resetMain()
     }
