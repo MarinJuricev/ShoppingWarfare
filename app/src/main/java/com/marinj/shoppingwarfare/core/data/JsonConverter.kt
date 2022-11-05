ce2001723f4abd155ca533873c1584e7938df583
MarinJ <mjuric00@fesb.hr>
2022-11-05 18:35:31 +0100

    Swap MoshiJsonConverter for JsonConverter that does not use an interface duo to inline/reified restrictions

--- app/src/main/java/com/marinj/shoppingwarfare/core/data/JsonConverter.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/core/data/JsonConverter.kt
@@ -1,6 +1,41 @@
 package com.marinj.shoppingwarfare.core.data
 
-interface JsonConverter {
+import kotlinx.serialization.json.Json
+import kotlinx.serialization.json.JsonArray
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.json.JsonNull
+import kotlinx.serialization.json.JsonObject
+import kotlinx.serialization.json.JsonPrimitive
+import kotlinx.serialization.json.decodeFromJsonElement
+import javax.inject.Inject
+import javax.inject.Singleton
 
-    fun <T> fromMapToObject(origin: Map<String, Any>): T
+// TODO: Can't wrap it into a interface since we need to rely on reified T to correctly pass
+//       the type to decodeFromJsonElement
+@Singleton
+class JsonConverter @Inject constructor(
+    val json: Json,
+) {
+
+    inline fun <reified T> fromMapToObject(
+        origin: Map<String, Any>,
+    ): T = origin.toJsonElement().let {
+        json.decodeFromJsonElement(it)
+    }
+
+    private fun Collection<*>.toJsonElement(): JsonElement =
+        JsonArray(mapNotNull { it.toJsonElement() })
+
+    fun Map<*, *>.toJsonElement(): JsonElement = JsonObject(
+        mapNotNull {
+            (it.key as? String ?: return@mapNotNull null) to it.value.toJsonElement()
+        }.toMap(),
+    )
+
+    private fun Any?.toJsonElement(): JsonElement = when (this) {
+        null -> JsonNull
+        is Map<*, *> -> toJsonElement()
+        is Collection<*> -> toJsonElement()
+        else -> JsonPrimitive(toString())
+    }
 }
