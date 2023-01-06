b0ca385facd5861ee5c96f7f405a0fb90fd1f6cc
marinjuricev <mjuric00@fesb.hr>
2023-01-06 21:36:28 +0100

    Add a basic fake navigator

---
+++ app/src/test/java/com/marinj/shoppingwarfare/core/fixture/AppFixtures.kt
@@ -0,0 +1,16 @@
+package com.marinj.shoppingwarfare.core.fixture
+
+import com.marinj.shoppingwarfare.core.navigation.NavigationEvent
+import com.marinj.shoppingwarfare.core.navigation.Navigator
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+
+object FakeNavigator : Navigator {
+
+    val receivedEvents = MutableSharedFlow<NavigationEvent>()
+    override val navigationEvent = receivedEvents.asSharedFlow()
+
+    override suspend fun emitDestination(navigationEvent: NavigationEvent) {
+        receivedEvents.emit(navigationEvent)
+    }
+}
