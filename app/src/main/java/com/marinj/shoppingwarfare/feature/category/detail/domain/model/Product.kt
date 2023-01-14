806c6e24a640f731626ebaba75437bcdd4c09fa7
marinjuricev <mjuric00@fesb.hr>
2023-01-14 14:15:40 +0100

    Make product have a private constructor and restrict it's creation

--- app/src/main/java/com/marinj/shoppingwarfare/feature/category/detail/domain/model/Product.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/category/detail/domain/model/Product.kt
@@ -1,8 +1,31 @@
 package com.marinj.shoppingwarfare.feature.category.detail.domain.model
 
-data class Product(
+import com.marinj.shoppingwarfare.core.result.Either
+import com.marinj.shoppingwarfare.core.result.Failure
+import com.marinj.shoppingwarfare.core.result.Failure.ErrorMessage
+import com.marinj.shoppingwarfare.core.result.buildLeft
+import com.marinj.shoppingwarfare.core.result.buildRight
+
+data class Product private constructor(
     val id: String,
     val categoryId: String,
     val categoryName: String,
     val name: String,
-)
+) {
+    companion object {
+        fun of(
+            id: String,
+            categoryId: String,
+            categoryName: String,
+            name: String,
+        ): Either<Failure, Product> = when {
+            name.isBlank() -> ErrorMessage("Name can not be empty got: $name").buildLeft()
+            else -> Product(
+                id = id,
+                categoryId = categoryId,
+                categoryName = categoryName,
+                name = name,
+            ).buildRight()
+        }
+    }
+}
