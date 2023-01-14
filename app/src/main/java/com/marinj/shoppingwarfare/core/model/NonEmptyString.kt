008310a86105c0b62ab05a2d51f3d38e2297412d
marinjuricev <mjuric00@fesb.hr>
2023-01-14 14:19:45 +0100

    Add NonEmptyString to offload the validation of our objects

---
+++ app/src/main/java/com/marinj/shoppingwarfare/core/model/NonEmptyString.kt
@@ -0,0 +1,17 @@
+package com.marinj.shoppingwarfare.core.model
+
+import com.marinj.shoppingwarfare.core.result.Either
+import com.marinj.shoppingwarfare.core.result.Failure
+import com.marinj.shoppingwarfare.core.result.Failure.ErrorMessage
+import com.marinj.shoppingwarfare.core.result.buildLeft
+import com.marinj.shoppingwarfare.core.result.buildRight
+
+@JvmInline
+value class NonEmptyString private constructor(val value: String) {
+    companion object {
+        fun of(value: String): Either<Failure, NonEmptyString> = when {
+            value.isBlank() -> ErrorMessage("Can not provide a value").buildLeft()
+            else -> NonEmptyString(value).buildRight()
+        }
+    }
+}
