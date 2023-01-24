f39608b4661d7e8957ae59bd69edc55962a3a755
marinjuricev <mjuric00@fesb.hr>
2023-01-24 23:30:55 +0100

    Add ProductApiImpl, the delete operation won't work for now we'll need to change the contract to send the whole product

--- app/src/main/java/com/marinj/shoppingwarfare/feature/category/detail/data/datasource/network/ProductApiImpl.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/category/detail/data/datasource/network/ProductApiImpl.kt
@@ -5,10 +5,15 @@ import com.marinj.shoppingwarfare.core.data.JsonConverter
 import com.marinj.shoppingwarfare.core.ext.addWarfareSnapshotListener
 import com.marinj.shoppingwarfare.core.result.Either
 import com.marinj.shoppingwarfare.core.result.Failure
+import com.marinj.shoppingwarfare.core.result.buildLeft
+import com.marinj.shoppingwarfare.core.result.buildRight
+import com.marinj.shoppingwarfare.core.result.toLeft
 import com.marinj.shoppingwarfare.feature.category.detail.data.model.RemoteProduct
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.callbackFlow
+import kotlinx.coroutines.suspendCancellableCoroutine
 import javax.inject.Inject
+import kotlin.coroutines.resume
 
 class ProductApiImpl @Inject constructor(
     private val fireStore: FirebaseFirestore,
@@ -33,12 +38,36 @@ class ProductApiImpl @Inject constructor(
         }
     }
 
-    override suspend fun addProduct(product: RemoteProduct): Either<Failure, Unit> {
-        TODO("Not yet implemented")
+    override suspend fun addProduct(
+        product: RemoteProduct,
+    ): Either<Failure, Unit> = suspendCancellableCoroutine { continuation ->
+        fireStore
+            .getProductDocument(product.categoryId)
+            .set(product)
+            .addOnSuccessListener {
+                if (continuation.isActive) {
+                    continuation.resume(Unit.buildRight())
+                }
+            }
+            .addOnFailureListener { exception: Exception ->
+                continuation.resume(exception.toLeft())
+            }
     }
 
-    override suspend fun deleteProductById(id: String): Either<Failure, Unit> {
-        TODO("Not yet implemented")
+    override suspend fun deleteProductById(
+        id: String,
+    ): Either<Failure, Unit> = suspendCancellableCoroutine { continuation ->
+        fireStore
+            .getProductDocument(categoryId = id)
+            .delete()
+            .addOnSuccessListener {
+                if (continuation.isActive) {
+                    continuation.resume(Unit.buildRight())
+                }
+            }
+            .addOnFailureListener {
+                continuation.resume(Failure.Unknown.buildLeft())
+            }
     }
 
     private fun FirebaseFirestore.getProductDocument(
