559fb3f2b8ac18b7da3db4264d467b981d500ad0
marinjuricev <mjuric00@fesb.hr>
2023-02-03 00:16:25 +0100

    Start writing out fakes for the product flow

--- app/src/test/java/com/marinj/shoppingwarfare/fixtures/category/ProductFixtures.kt
+++ app/src/test/java/com/marinj/shoppingwarfare/fixtures/category/ProductFixtures.kt
@@ -1,11 +1,18 @@
 package com.marinj.shoppingwarfare.fixtures.category
 
+import com.marinj.shoppingwarfare.core.result.Either
+import com.marinj.shoppingwarfare.core.result.Failure
+import com.marinj.shoppingwarfare.core.result.buildRight
 import com.marinj.shoppingwarfare.core.result.takeRightOrNull
+import com.marinj.shoppingwarfare.feature.category.detail.data.datasource.local.ProductDao
+import com.marinj.shoppingwarfare.feature.category.detail.data.datasource.network.ProductApi
 import com.marinj.shoppingwarfare.feature.category.detail.data.model.LocalCategoryProducts
 import com.marinj.shoppingwarfare.feature.category.detail.data.model.LocalProduct
 import com.marinj.shoppingwarfare.feature.category.detail.data.model.RemoteProduct
 import com.marinj.shoppingwarfare.feature.category.detail.domain.model.Product
 import com.marinj.shoppingwarfare.feature.category.list.data.model.LocalCategory
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flow
 
 fun buildRemoteProduct(
     providedProductId: String = "",
@@ -51,24 +58,54 @@ fun buildProduct(
     name = providedName,
 ).takeRightOrNull()!!
 
-//class FakeSuccessProductDao(
-//    private val localProductsToReturn: MutableList<LocalProduct> = mutableListOf(),
-//) : ProductDao {
-//
-//    override fun observeProductsForGivenCategoryId(
-//        categoryId: String,
-//    ): Flow<List<LocalCategoryProducts>> = flow {
-//        emit(localProductsToReturn)
-//    }
-//
-//    override suspend fun upsertProduct(entity: LocalProduct): Long {
-//        TODO("Not yet implemented")
-//    }
-//
-//    override suspend fun deleteProductById(productId: String) {
-//        TODO("Not yet implemented")
-//    }
-//
-//}
+class FakeSuccessProductDao(
+    private val localProductsToEmit: List<LocalCategoryProducts> = emptyList(),
+) : ProductDao {
+
+    val localProducts = mutableListOf<LocalProduct>()
+
+    override fun observeProductsForGivenCategoryId(
+        categoryId: String,
+    ): Flow<List<LocalCategoryProducts>> = flow {
+        emit(localProductsToEmit)
+    }
+
+    override suspend fun upsertProduct(
+        entity: LocalProduct,
+    ): Long {
+        localProducts.add(entity)
+        return 1L
+    }
+
+    override suspend fun deleteProductById(productId: String) {
+        localProducts.removeIf { it.productId == productId }
+    }
+}
+
+class FakeSuccessProductApi(
+    private val productsToEmit: List<RemoteProduct> = listOf(buildRemoteProduct()),
+) : ProductApi {
+
+    val remoteProducts = mutableListOf<RemoteProduct>()
+    override fun observeProductsForGivenCategoryId(
+        categoryId: String,
+    ): Flow<List<RemoteProduct>> = flow {
+        emit(productsToEmit)
+    }
+
+    override suspend fun addProduct(
+        product: RemoteProduct,
+    ): Either<Failure, Unit> {
+        remoteProducts.add(product)
+        return Unit.buildRight()
+    }
+
+    override suspend fun deleteProductById(
+        id: String,
+    ): Either<Failure, Unit> {
+        remoteProducts.removeIf { it.productId == id }
+        return Unit.buildRight()
+    }
+}
 
 private const val PRODUCT_NAME = "productName"
