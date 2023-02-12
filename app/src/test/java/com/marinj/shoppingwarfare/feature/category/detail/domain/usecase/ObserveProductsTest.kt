7eb47ba91eadef859450c098ac2a1c394e44c0b5
marinjuricev <mjuric00@fesb.hr>
2023-02-12 23:44:28 +0100

    Start rewriting product test coverages to fakes

--- app/src/test/java/com/marinj/shoppingwarfare/feature/category/detail/domain/usecase/ObserveProductsTest.kt
+++ app/src/test/java/com/marinj/shoppingwarfare/feature/category/detail/domain/usecase/ObserveProductsTest.kt
@@ -3,10 +3,8 @@ package com.marinj.shoppingwarfare.feature.category.detail.domain.usecase
 import app.cash.turbine.test
 import com.google.common.truth.Truth.assertThat
 import com.marinj.shoppingwarfare.feature.category.detail.domain.repository.ProductRepository
+import com.marinj.shoppingwarfare.fixtures.category.FakeSuccessProductRepository
 import com.marinj.shoppingwarfare.fixtures.category.buildProduct
-import io.mockk.coEvery
-import io.mockk.mockk
-import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
@@ -15,7 +13,13 @@ private const val CATEGORY_ID = "id"
 
 class ObserveProductsTest {
 
-    private val productRepository: ProductRepository = mockk()
+    private val products = listOf(
+        buildProduct(
+            providedCategoryName = CATEGORY_NAME,
+            providedName = PRODUCT_NAME,
+        ),
+    )
+    private val productRepository: ProductRepository = FakeSuccessProductRepository(products)
 
     private lateinit var sut: ObserveProducts
 
@@ -27,25 +31,13 @@ class ObserveProductsTest {
     }
 
     @Test
-    fun `invoke should return result from categoryDetailRepository observeCategoryItems`() =
-        runTest {
-            val product = buildProduct(
-                providedCategoryName = CATEGORY_NAME,
-                providedName = PRODUCT_NAME,
-            )
-            val products = listOf(product)
-            val categoryItemFlow = flow {
-                emit(products)
-            }
-            coEvery {
-                productRepository.observeProducts(CATEGORY_ID)
-            } coAnswers { categoryItemFlow }
+    fun `invoke SHOULD return result from repository`() = runTest {
 
-            sut(CATEGORY_ID).test {
-                assertThat(awaitItem()).isEqualTo(products)
-                awaitComplete()
-            }
+        sut(CATEGORY_ID).test {
+            assertThat(awaitItem()).isEqualTo(products)
+            awaitComplete()
         }
+    }
 }
 
 private const val CATEGORY_NAME = "categoryName"
