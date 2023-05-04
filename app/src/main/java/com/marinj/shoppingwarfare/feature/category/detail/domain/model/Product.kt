be21dbea2a4ed5aef8ca85815605668508db0768
marinjuricev <mjuric00@fesb.hr>
2023-05-05 00:27:20 +0200

    Make Product also into a private DDD object that follows a similar pattern as Category

--- app/src/main/java/com/marinj/shoppingwarfare/feature/category/detail/domain/model/Product.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/category/detail/domain/model/Product.kt
@@ -1,35 +1,35 @@
 package com.marinj.shoppingwarfare.feature.category.detail.domain.model
 
 import arrow.core.Either
-import arrow.core.left
-import arrow.core.right
+import arrow.core.raise.either
+import com.marinj.shoppingwarfare.core.model.NonEmptyString
+import com.marinj.shoppingwarfare.core.model.NonEmptyString.Companion.NonEmptyString
 import com.marinj.shoppingwarfare.core.result.Failure
-import com.marinj.shoppingwarfare.core.result.Failure.ErrorMessage
 
-data class Product(
-    val id: String,
-    val categoryId: String,
-    val categoryName: String,
-    val name: String,
+data class Product private constructor(
+    val id: NonEmptyString,
+    val categoryId: NonEmptyString,
+    val categoryName: NonEmptyString,
+    val name: NonEmptyString,
 ) {
     companion object {
-        fun of(
+        fun Product(
             id: String,
             categoryId: String,
             categoryName: String,
             name: String?,
-        ): Either<Failure, Product> {
-            return when {
-                name.isNullOrBlank() -> ErrorMessage("Name can not be empty got: $name").left()
-                categoryName.isBlank() -> ErrorMessage("CategoryName can not be empty got: $categoryName").left()
-                categoryId.isBlank() -> ErrorMessage("CategoryId can not be empty got: $categoryId").left()
-                else -> Product(
-                    id = id,
-                    categoryId = categoryId,
-                    categoryName = categoryName,
-                    name = name,
-                ).right()
-            }
+        ): Either<Failure, Product> = either {
+            val mappedId = NonEmptyString(valueToValidate = id, tag = "id").bind()
+            val mappedCategoryId = NonEmptyString(valueToValidate = categoryId, tag = "categoryId").bind()
+            val mappedCategoryName = NonEmptyString(valueToValidate = categoryName, tag = "categoryName").bind()
+            val mappedName = NonEmptyString(valueToValidate = name, tag = "name").bind()
+
+            Product(
+                id = mappedId,
+                categoryId = mappedCategoryId,
+                categoryName = mappedCategoryName,
+                name = mappedName,
+            )
         }
     }
 }
