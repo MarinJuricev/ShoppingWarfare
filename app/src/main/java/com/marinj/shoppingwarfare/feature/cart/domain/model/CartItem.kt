37f595489f16fbcb223e8d97bf8c201bb8d6cf2b
marinjuricev <mjuric00@fesb.hr>
2023-05-15 21:25:09 +0200

    Migrate the CartItem to provide NonEmptyString and private constructor approach

--- app/src/main/java/com/marinj/shoppingwarfare/feature/cart/domain/model/CartItem.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/cart/domain/model/CartItem.kt
@@ -1,12 +1,40 @@
 package com.marinj.shoppingwarfare.feature.cart.domain.model
 
-data class CartItem(
-    val id: String,
-    val categoryName: String,
-    val name: String,
-    val quantity: Int = DEFAULT_QUANTITY,
-    val isInBasket: Boolean = DEFAULT_IS_IN_BASKET,
-)
+import arrow.core.Either
+import arrow.core.raise.either
+import com.marinj.shoppingwarfare.core.model.NonEmptyString
+import com.marinj.shoppingwarfare.core.model.NonEmptyString.Companion.NonEmptyString
+import com.marinj.shoppingwarfare.core.result.Failure
 
-private const val DEFAULT_QUANTITY = 1
+data class CartItem private constructor(
+    val id: NonEmptyString,
+    val categoryName: NonEmptyString,
+    val name: NonEmptyString,
+    val quantity: UInt = DEFAULT_QUANTITY,
+    val isInBasket: Boolean = DEFAULT_IS_IN_BASKET,
+) {
+    companion object {
+        fun CartItem(
+            id: String,
+            categoryName: String,
+            name: String,
+            quantity: UInt? = null,
+            isInBasket: Boolean? = false,
+        ): Either<Failure, CartItem> = either {
+            val mappedId = NonEmptyString(valueToValidate = id, tag = "id").bind()
+            val mappedCategoryName = NonEmptyString(valueToValidate = categoryName, tag = "categoryName").bind()
+            val mappedName = NonEmptyString(valueToValidate = name, tag = "name").bind()
+
+            CartItem(
+                id = mappedId,
+                categoryName = mappedCategoryName,
+                name = mappedName,
+                quantity = quantity ?: DEFAULT_QUANTITY,
+                isInBasket = isInBasket ?: DEFAULT_IS_IN_BASKET,
+            )
+        }
+    }
+}
+
+private const val DEFAULT_QUANTITY: UInt = 1u
 private const val DEFAULT_IS_IN_BASKET = false
