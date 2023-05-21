ba58c0fe9e22a010c1fa4c32511320e78c4f1cf3
marinjuricev <mjuric00@fesb.hr>
2023-05-21 23:35:28 +0200

    Provide a fake cart dao

--- app/src/test/java/com/marinj/shoppingwarfare/feature/cart/CartFixtures.kt
+++ app/src/test/java/com/marinj/shoppingwarfare/feature/cart/CartFixtures.kt
@@ -1,7 +1,10 @@
 package com.marinj.shoppingwarfare.feature.cart
 
+import com.marinj.shoppingwarfare.feature.cart.data.datasource.CartDao
 import com.marinj.shoppingwarfare.feature.cart.data.model.LocalCartItem
 import com.marinj.shoppingwarfare.feature.cart.domain.model.CartItem.Companion.CartItem
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.flowOf
 
 fun buildCartItem(
     providedId: String = ID,
@@ -30,6 +33,27 @@ fun buildLocalCartItem(
     quantity = providedQuantity,
     isInBasket = providedIsInBasket,
 )
+
+class FakeSuccessCartDao(
+    private val cartListToReturn: List<LocalCartItem> = listOf(buildLocalCartItem()),
+) : CartDao {
+
+    override fun observeCartItems(): Flow<List<LocalCartItem>> = flowOf(cartListToReturn)
+
+    override fun observeCartItemsCount(): Flow<Int?> = flowOf(cartListToReturn.size)
+
+    override suspend fun updateCartItemQuantity(id: String, newQuantity: Int) = Unit
+
+    override suspend fun updateCartItemIsInBasket(id: String, updatedIsInBasket: Boolean) = Unit
+
+    override suspend fun upsertCartItem(entity: LocalCartItem): Long = 1L
+
+    override suspend fun deleteCartItemById(id: String) = Unit
+
+    override suspend fun getCartItemById(id: String): LocalCartItem? = buildLocalCartItem()
+
+    override suspend fun deleteCart() = Unit
+}
 
 private const val ID = "ID"
 private const val CATEGORY_NAME = "CATEGORY_NAME"
