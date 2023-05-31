bb9d55e3d4bd8188f920271670e5e3870da23923
marinjuricev <mjuric00@fesb.hr>
2023-05-31 23:18:33 +0200

    Refactor UiCartItemToCartItemMapper to take use of the domain CartItem

--- app/src/main/java/com/marinj/shoppingwarfare/feature/cart/presentation/mapper/UiCartItemToCartItemMapper.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/cart/presentation/mapper/UiCartItemToCartItemMapper.kt
@@ -1,24 +1,23 @@
 package com.marinj.shoppingwarfare.feature.cart.presentation.mapper
 
 import com.marinj.shoppingwarfare.feature.cart.domain.model.CartItem
+import com.marinj.shoppingwarfare.feature.cart.domain.model.CartItem.Companion.CartItem
 import com.marinj.shoppingwarfare.feature.cart.presentation.model.UiCartItem
 import javax.inject.Inject
 
 class UiCartItemToCartItemMapper @Inject constructor() {
 
-    fun map(origin: List<UiCartItem>): List<CartItem> {
-        return origin
-            .filterIsInstance(UiCartItem.Content::class.java)
-            .map { uiCartItem ->
-                with(uiCartItem) {
-                    CartItem(
-                        id = id,
-                        categoryName = categoryName,
-                        name = name,
-                        quantity = quantity,
-                        isInBasket = isInBasket,
-                    )
-                }
+    fun map(origin: List<UiCartItem>): List<CartItem> = origin
+        .filterIsInstance(UiCartItem.Content::class.java)
+        .mapNotNull { uiCartItem ->
+            with(uiCartItem) {
+                CartItem(
+                    id = id,
+                    categoryName = categoryName,
+                    name = name,
+                    quantity = quantity.toUInt(),
+                    isInBasket = isInBasket,
+                ).getOrNull()
             }
-    }
+        }
 }
