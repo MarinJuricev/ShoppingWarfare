655e9ae8fcf9117d83c78773a27ff25222e2d2da
marinjuricev <mjuric00@fesb.hr>
2023-06-05 22:51:45 +0200

    Refactor tests coverage to use fakes, instead of mocks

--- app/src/test/java/com/marinj/shoppingwarfare/feature/cart/presentation/mapper/CartItemToUiCartItemMapperTest.kt
+++ app/src/test/java/com/marinj/shoppingwarfare/feature/cart/presentation/mapper/CartItemToUiCartItemMapperTest.kt
@@ -1,56 +1,49 @@
 package com.marinj.shoppingwarfare.feature.cart.presentation.mapper
 
 import com.google.common.truth.Truth.assertThat
+import com.marinj.shoppingwarfare.feature.cart.buildCartItem
 import com.marinj.shoppingwarfare.feature.cart.domain.model.CartItem
 import com.marinj.shoppingwarfare.feature.cart.presentation.model.UiCartItem
+import com.marinj.shoppingwarfare.feature.cart.presentation.model.UiCartItem.*
 import io.mockk.every
 import io.mockk.mockk
+import kotlinx.coroutines.test.runTest
 import org.junit.Before
 import org.junit.Test
 
-private const val CATEGORY_NAME = "categoryName"
-private const val ID = "id"
-private const val NAME = "name`"
-private const val QUANTITY = 5
-private const val IS_IN_BASKET = false
 
 class CartItemToUiCartItemMapperTest {
 
-    private lateinit var sut: CartItemToUiCartItemMapper
-
-    @Before
-    fun setUp() {
-        sut = CartItemToUiCartItemMapper()
-    }
+    private val sut = CartItemToUiCartItemMapper()
 
     @Test
-    fun `map SHOULD return UiCartItem Header as the first item in the list`() {
-        val cartItems = mockk<CartItem>().apply {
-            every { categoryName } returns CATEGORY_NAME
-            every { id } returns ID
-            every { name } returns NAME
-            every { quantity } returns QUANTITY
-            every { isInBasket } returns IS_IN_BASKET
-        }
+    fun `map SHOULD return UiCartItem Header as the first item in the list`() = runTest {
+        val cartItem = buildCartItem(
+            providedCategoryName = CATEGORY_NAME,
+            providedId = ID,
+            providedName = NAME,
+            providedQuantity = QUANTITY.toUInt(),
+            providedIsInBasket = IS_IN_BASKET,
+        )
 
-        val result = sut.map(listOf(cartItems))
-        val expectedResult = UiCartItem.Header(id = CATEGORY_NAME, categoryName = CATEGORY_NAME)
+        val result = sut.map(listOf(cartItem))
+        val expectedResult = Header(id = CATEGORY_NAME, categoryName = CATEGORY_NAME)
 
         assertThat(result.first()).isEqualTo(expectedResult)
     }
 
     @Test
-    fun `map SHOULD return UiCartItem Content as the second item in the list`() {
-        val cartItems = mockk<CartItem>().apply {
-            every { categoryName } returns CATEGORY_NAME
-            every { id } returns ID
-            every { name } returns NAME
-            every { quantity } returns QUANTITY
-            every { isInBasket } returns IS_IN_BASKET
-        }
+    fun `map SHOULD return UiCartItem Content as the second item in the list`() = runTest {
+        val cartItem = buildCartItem(
+            providedCategoryName = CATEGORY_NAME,
+            providedId = ID,
+            providedName = NAME,
+            providedQuantity = QUANTITY.toUInt(),
+            providedIsInBasket = IS_IN_BASKET,
+        )
 
-        val result = sut.map(listOf(cartItems))
-        val expectedResult = UiCartItem.Content(
+        val result = sut.map(listOf(cartItem))
+        val expectedResult = Content(
             id = ID,
             categoryName = CATEGORY_NAME,
             name = NAME,
@@ -61,3 +54,9 @@ class CartItemToUiCartItemMapperTest {
         assertThat(result[1]).isEqualTo(expectedResult)
     }
 }
+
+private const val CATEGORY_NAME = "categoryName"
+private const val ID = "id"
+private const val NAME = "name`"
+private const val QUANTITY = 5
+private const val IS_IN_BASKET = false
