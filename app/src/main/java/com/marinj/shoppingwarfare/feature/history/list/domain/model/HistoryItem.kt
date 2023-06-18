458012093fce141bb405c5a5a005ea17c8c49648
marinjuricev <mjuric00@fesb.hr>
2023-06-18 22:29:25 +0200

    Add DDD HistoryItem

--- app/src/main/java/com/marinj/shoppingwarfare/feature/history/list/domain/model/HistoryItem.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/history/list/domain/model/HistoryItem.kt
@@ -1,9 +1,35 @@
 package com.marinj.shoppingwarfare.feature.history.list.domain.model
 
-data class HistoryItem(
-    val id: String,
-    val receiptPath: String?,
+import arrow.core.raise.either
+import com.marinj.shoppingwarfare.core.model.NonEmptyString
+import com.marinj.shoppingwarfare.core.model.NonEmptyString.Companion.NonEmptyString
+
+data class HistoryItem private constructor(
+    val id: NonEmptyString,
+    val receiptPath: NonEmptyString?,
     val timestamp: Long,
-    val cartName: String,
+    val cartName: NonEmptyString,
     val historyCartItems: List<HistoryCartItem>,
-)
+) {
+    companion object {
+        fun HistoryItem(
+            id: String,
+            receiptPath: String?,
+            timestamp: Long,
+            cartName: String,
+            historyCartItems: List<HistoryCartItem>,
+        ) = either {
+            val mappedId = NonEmptyString(valueToValidate = id, tag = "id").bind()
+            val mappedReceiptPath = NonEmptyString(valueToValidate = receiptPath, tag = "receiptPath").bind()
+            val mappedCartName = NonEmptyString(valueToValidate = cartName, tag = "cartName").bind()
+
+            HistoryItem(
+                id = mappedId,
+                receiptPath = mappedReceiptPath,
+                timestamp = timestamp,
+                cartName = mappedCartName,
+                historyCartItems = historyCartItems,
+            )
+        }
+    }
+}
