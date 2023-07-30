57db24073bf24a1f6cf8b69369faa5733f4a1a03
marinjuricev <mjuric00@fesb.hr>
2023-07-30 13:34:36 +0200

    Add the Material 3 dependency

--- design-system/build.gradle.kts
+++ design-system/build.gradle.kts
@@ -34,10 +34,14 @@ android {
 dependencies {
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.util)
-    implementation(libs.androidx.compose.material)
+//    implementation(libs.androidx.compose.material)
     implementation(libs.androidx.compose.runtime)
     implementation(libs.androidx.compose.animation)
     implementation(libs.androidx.compose.foundation)
     implementation(libs.androidx.compose.compiler)
+
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.window)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
 }
