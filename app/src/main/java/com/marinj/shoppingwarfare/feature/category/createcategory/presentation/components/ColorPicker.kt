89b4bce51924a0fd3c0d814153f8dc49646e26f9
marinjuricev <mjuric00@fesb.hr>
2023-08-08 21:50:14 +0200

    Address lint issues

--- app/src/main/java/com/marinj/shoppingwarfare/feature/category/createcategory/presentation/components/ColorPicker.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/feature/category/createcategory/presentation/components/ColorPicker.kt
@@ -12,7 +12,6 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.itemsIndexed
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
