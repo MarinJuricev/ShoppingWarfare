b6892bcb79f06f40f59f30aadd74944a97aadafa
marinjuricev <mjuric00@fesb.hr>
2023-09-19 22:54:57 +0200

    Add test coverage for toRemote for RemoteCartItem

--- app/src/test/java/com/marinj/shoppingwarfare/feature/cart/data/model/RemoteCartItemTest.kt
+++ app/src/test/java/com/marinj/shoppingwarfare/feature/cart/data/model/RemoteCartItemTest.kt
@@ -1,5 +1,7 @@
 package com.marinj.shoppingwarfare.feature.cart.data.model
 
+import com.marinj.shoppingwarfare.feature.cart.buildCartItem
+import com.marinj.shoppingwarfare.feature.cart.buildLocalCartItem
 import com.marinj.shoppingwarfare.feature.cart.buildRemoteCartItem
 import org.junit.Assert.assertEquals
 import org.junit.Test
@@ -64,6 +66,66 @@ class RemoteCartItemTest {
         val actual = sut.toLocal()
 
         assertEquals(expectedResult, actual.isInBasket)
+    }
+
+    @Test
+    fun `toRemote SHOULD map cartItemId`() {
+        val expectedResult = CART_ITEM_ID
+        val sut = buildCartItem(
+            providedId = expectedResult,
+        )
+
+        val actual = sut.toRemote()
+
+        assertEquals(expectedResult, actual.cartItemId)
+    }
+
+    @Test
+    fun `toRemote SHOULD map categoryName`() {
+        val expectedResult = CATEGORY_NAME
+        val sut = buildCartItem(
+            providedCategoryName = expectedResult,
+        )
+
+        val actual = sut.toRemote()
+
+        assertEquals(expectedResult, actual.categoryName)
+    }
+
+    @Test
+    fun `toRemote SHOULD map name`(){
+        val expectedResult = NAME
+        val sut = buildCartItem(
+            providedName = expectedResult,
+        )
+
+        val actual = sut.toRemote()
+
+        assertEquals(expectedResult, actual.name)
+    }
+
+    @Test
+    fun `toRemote SHOULD map quantity`() {
+        val expectedResult = QUANTITY
+        val sut = buildCartItem(
+            providedQuantity = expectedResult,
+        )
+
+        val actual = sut.toRemote()
+
+        assertEquals(expectedResult.toInt(), actual.quantity)
+    }
+
+    @Test
+    fun `toRemote SHOULD map isInBasket`() {
+        val expectedResult = IN_BASKET
+        val sut = buildCartItem(
+            providedIsInBasket = expectedResult,
+        )
+
+        val actual = sut.toRemote()
+
+        assertEquals(expectedResult, actual.inBasket)
     }
 }
 
