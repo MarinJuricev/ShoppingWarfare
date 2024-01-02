b03deab5fced2ab3cf75ce70b236f66962f19542
marinjuricev <mjuric00@fesb.hr>
2024-01-02 21:46:21 +0100

    Add the new productDao

--- app/src/main/java/com/marinj/shoppingwarfare/core/data/ShoppingWarfareDatabase.kt
+++ app/src/main/java/com/marinj/shoppingwarfare/core/data/ShoppingWarfareDatabase.kt
@@ -1,9 +1,11 @@
 package com.marinj.shoppingwarfare.core.data
 
 import com.marinj.shoppingwarfare.feature.cart.data.datasource.CartDao
+import com.marinj.shoppingwarfare.feature.category.detail.data.datasource.local.ProductDao
 import com.marinj.shoppingwarfare.feature.history.list.data.datasource.HistoryDao
 
 interface ShoppingWarfareDatabase {
     fun cartDao(): CartDao
     fun historyDao(): HistoryDao
+    fun productDao(): ProductDao
 }
